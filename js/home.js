f737e8374fc863939fd0904dc87efa05c2e1ed7d
Andželika <94860498+zazeli@users.noreply.github.com>
2022-01-05 19:43:33 +0200

    Initial Commit

---
+++ js/home.js
@@ -0,0 +1,8 @@
+const pregressBarGroup = document.querySelector ('.progress-bar-group');
+const optionsDOM = pregressBarGroupDOM.querySelector ('.option');
+
+for (const optionDOM of optionsDOM) {
+    optionsDOM.addEventListener ('click', () =>) {
+        const groupIndex = OptionDOM.dataset.group = optionDOM
+    }
+}
